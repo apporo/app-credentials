6e48846c2ebc8ca1ccd273899d2a470b8b5c89f8
pnhung177 <pnhung177@acegik.net>
2018-07-09 14:22:49 +0700

    Refactoring buildPermissionChecker() method

--- src/services/checker.js
+++ src/services/checker.js
@@ -38,28 +38,36 @@ function Checker(params) {
     permissionExtractor = function (req) { return null; }
   }
 
-  this.buildPermissionChecker = function (express) {
-    let router = express.Router();
-
-    router.all('*', function (req, res, next) {
-      for (let i = 0; i < compiledRules.length; i++) {
-        let rule = compiledRules[i];
-        if (req.url.match(rule.urlPattern)) {
-          if (lodash.isEmpty(rule.methods) || (rule.methods.indexOf(req.method) >= 0)) {
-            let permissions = permissionExtractor(req);
-            L.has('silly') && L.log('silly', 'extracted permissions: %s', JSON.stringify(permissions));
-            if (lodash.isEmpty(rule.permission) || (lodash.isArray(permissions) && permissions.indexOf(rule.permission) >= 0)) {
-              L.has('silly') && L.log('silly', 'permission accepted: %s', rule.permission);
-              return next();
-            } else {
-              return res.status(403).json({ success: false, message: 'Insufficient permission to grant access' });
-            }
+  this.checkPermissions = function(req) {
+    let checked = false, passed = null;
+    for (let i = 0; i < compiledRules.length; i++) {
+      let rule = compiledRules[i];
+      if (req.url && req.url.match(rule.urlPattern)) {
+        if (lodash.isEmpty(rule.methods) || (req.method && rule.methods.indexOf(req.method) >= 0)) {
+          let permissions = permissionExtractor(req);
+          L.has('silly') && L.log('silly', 'extracted permissions: %s', JSON.stringify(permissions));
+          if (lodash.isEmpty(rule.permission) || (lodash.isArray(permissions) && permissions.indexOf(rule.permission) >= 0)) {
+            L.has('silly') && L.log('silly', 'permission accepted: %s', rule.permission);
+            passed = true;
+          } else {
+            passed = false;
           }
+          checked = true;
+          break;
         }
       }
-      return next();
-    });
+    }
+    return passed;
+  }
 
+  this.buildPermissionChecker = function (express) {
+    let router = express.Router();
+    router.all('*', function (req, res, next) {
+      let passed = self.checkPermissions(req);
+      if (passed === null) return next();
+      if (passed) return next();
+      return res.status(403).json({ success: false, message: 'Insufficient permission to grant access' });
+    });
     return router;
   };
 };
